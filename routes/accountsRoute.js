fbc8455d50271b249c28ef1799c74709dd89e14d
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-08-12 16:59:22 -0700

    Project initialized. Installed dependencies

---
+++ routes/accountsRoute.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+
+// database access using knex
+const db = require('../data/dbConfig');
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
+const server = express();
 const db = require('./data/dbConfig.js');
 
-const server = express();
-
 server.use(express.json());
+
+// routes
+const accountsRoute = require('./routes/accountsRoute');
+
+// api check
+server.get('/', (req, res) => {
+	res.send(`<h1>API is running</h1>`);
+});
 
 module.exports = server;
