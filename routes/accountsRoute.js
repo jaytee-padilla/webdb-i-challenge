6a4029e75988feba9cb20e95d6446c5bc1671484
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-08-12 17:56:55 -0700

    Added ability to GET all accounts data and specific accounts by id

--- routes/accountsRoute.js
+++ routes/accountsRoute.js
@@ -3,3 +3,42 @@ const router = express.Router();
 
 // database access using knex
 const db = require('../data/dbConfig');
+
+// CRUD
+// GET
+// get list of accounts
+router.get('/', (req, res) => {
+	db('accounts')
+		.then(accounts => {
+			res.status(200).json(accounts);
+		})
+		.catch(error => {
+			res.status(500).json({message: 'Error getting posts from database'});
+		});
+});
+
+// get specific account by ID
+router.get('/:id', (req, res) => {
+	db('accounts').where({id: req.params.id}).first()
+		.then(account => {
+			if(account) {
+				res.status(200).json(account);
+			} else{
+				res.status(404).json({message: 'That account ID does not exist'})
+			}
+		})
+		.catch(error => {
+			res.status(500).json({message: 'Error getting posts from database'});
+		});
+});
+
+// POST
+// add new account
+
+// DELETE
+// delete specific account by id
+
+// PUT
+// update specific account by id
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ server.use(express.json());
 
 // routes
 const accountsRoute = require('./routes/accountsRoute');
+server.use('/accounts', accountsRoute);
 
 // api check
 server.get('/', (req, res) => {
